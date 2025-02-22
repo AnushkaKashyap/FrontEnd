100db9af39ff669237bf903b73ef2b43b8d03049
AnushkaKashyap <84405298+AnushkaKashyap@users.noreply.github.com>
2025-02-22 14:09:17 +0530

    Create add2nums.py
    Python Testing

---
+++ add2nums.py
@@ -0,0 +1,9 @@
+# Get input from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Add the numbers
+result = num1 + num2
+
+# Print the result
+print("The sum is:", result)
