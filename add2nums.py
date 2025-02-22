33048cf42bce9bcc0fc0106037cb25f71de1891c
AnushkaKashyap <84405298+AnushkaKashyap@users.noreply.github.com>
2025-02-22 14:24:00 +0530

    Update removing comments

--- add2nums.py
+++ add2nums.py
@@ -1,9 +1,6 @@
-# Get input from the user
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 
-# Add the numbers
 result = num1 + num2
 
-# Print the result
 print("The sum is:", result)
